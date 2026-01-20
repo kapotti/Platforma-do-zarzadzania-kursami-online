509ff529dad6388acd8e4fef1c47c60fd84b24c3
Jakub <jakubjozefczyk.kontakt@gmail.com>
2026-01-20 20:14:31 +0100

    Dodanie CRUD, walidacji, strona

--- KursyOnline/Data/ApplicationDbContext.cs
+++ KursyOnline/Data/ApplicationDbContext.cs
@@ -14,5 +14,16 @@ namespace KursyOnline.Data
         public DbSet<Course> Courses { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Lesson> Lessons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); 
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Programowanie" },
+                new Category { Id = 2, Name = "Grafika Komputerowa" },
+                new Category { Id = 3, Name = "Marketing" }
+            );
+        }
     }
 }
--- KursyOnline/Data/Migrations/20260120165224_InitialCreate.Designer.cs
+++ KursyOnline/Data/Migrations/20260120165224_InitialCreate.Designer.cs
@@ -4,6 +4,7 @@ using KursyOnline.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KursyOnline.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260120165224_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -38,23 +41,6 @@ namespace KursyOnline.Data.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Categories");
-
-                    b.HasData(
-                        new
-                        {
-                            Id = 1,
-                            Name = "Programowanie"
-                        },
-                        new
-                        {
-                            Id = 2,
-                            Name = "Grafika Komputerowa"
-                        },
-                        new
-                        {
-                            Id = 3,
-                            Name = "Marketing"
-                        });
                 });
 
             modelBuilder.Entity("KursyOnline.Models.Course", b =>
